531c65481a2be780a18458ad010d54851a5947e0
oneRain <onerain88@gmail.com>
2022-12-11 11:05:21 +0800

    chore: universal device info

--- UnityProject/Assets/Scenes/Sample.cs
+++ UnityProject/Assets/Scenes/Sample.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Sample : MonoBehaviour {
-    [DllImport("MacDeviceInfo", CallingConvention = CallingConvention.Cdecl)]
-    private static extern IntPtr GetDeviceLocation();
-
-    [DllImport("MacDeviceInfo", CallingConvention = CallingConvention.Cdecl)]
-    private static extern IntPtr GetDeviceLanguage();
-
     void Start() {
-        Debug.Log(Marshal.PtrToStringAnsi(GetDeviceLocation()));
-        Debug.Log(Marshal.PtrToStringAnsi(GetDeviceLanguage()));
+        Debug.Log($"location: {StandaloneDeviceInfo.GetLocation()}");
+        Debug.Log($"language: {StandaloneDeviceInfo.GetLanguage()}");
     }
 }
